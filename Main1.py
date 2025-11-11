63dcd3fe9ff89f61fdf26c89753e4de3e14c3367
Elizabeth-124 <elizabeth124.1234@gmail.com>
2025-11-11 20:00:17 +0000

    first commit

---
+++ Main1.py
@@ -0,0 +1,16 @@
+x=float(input("Enter a number"))
+y=float(input("Enter a second number"))
+sum=x+y
+difference=x-y
+product=x*y
+divide=x/y
+floor_division=x//y
+exp=x**2
+modulous=x%y
+print(f"The sum of {x} and {y} is {sum}")
+print(f"The difference between {x} and {y} is {difference}")
+print(f"The product of {x} and {y} is {product}")
+print(f"The division of {x} and {y} is {divide}")
+print(f"The floor division of {x} and {y}{floor_division}")
+print(f"The exponential of {x} and {y} is {exp}")
+print(f"The modulous of {x} and {y} is {modulous}")
